eaac09e91ab6f3c6d7dfd4604d4bc724edea0d6b
Your Name <you@example.com>
2018-09-21 16:15:22 +0200

    leaderBoard /wip

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,9 @@ import 'package:http/http.dart';
 import 'package:flutter_udid/flutter_udid.dart';
 
 void main() => runApp(new MyApp());
+
 Future<Post> fetchPost() async {
-  final response =
-  await get('https://jsonplaceholder.typicode.com/posts/1');
+  final response = await get('https://jsonplaceholder.typicode.com/posts/1');
 
   if (response.statusCode == 200) {
     // If the call to the server was successful, parse the JSON
@@ -39,7 +39,6 @@ class Post {
   }
 }
 
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -63,6 +62,56 @@ class MyApp extends StatelessWidget {
 }
 
 class LeaderBoard extends StatelessWidget {
+  /* Widget bodyData() =>
+      DataTable(
+          onSelectAll: (b) {},
+          sortColumnIndex: 1,
+          sortAscending: true,
+          columns: <DataColumn>[
+
+            DataColumn(
+              label: Text("First Name"),
+              numeric: false,
+              onSort: (i, b) {
+                print("$i $b");
+                setState(() {
+                  names.sort((a, b) => a.firstName.compareTo(b.firstName));
+                });
+              },
+              tooltip: "To display first name of the Name",
+            ),
+            DataColumn(
+              label: Text("Last Name"),
+              numeric: false,
+              onSort: (i, b) {
+                print("$i $b");
+                setState(() {
+                  names.sort((a, b) => a.lastName.compareTo(b.lastName));
+                });
+              },
+              tooltip: "To display last name of the Name",
+            ),
+          ],
+          rows: names
+              .map(
+                (name) =>
+                DataRow(
+                  cells: [
+                    DataCell(
+                      Text(name.firstName),
+                      showEditIcon: false,
+                      placeholder: false,
+                    ),
+                    DataCell(
+                      Text(name.lastName),
+                      showEditIcon: false,
+                      placeholder: false,
+                    )
+                  ],
+                ),
+          )
+              .toList()); */
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,19 +119,32 @@ class LeaderBoard extends StatelessWidget {
         title: Text("LeaderBoard"),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
+        child: FutureBuilder<Post>(
+          future: fetchPost(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data.body);
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+
+            // By default, show a loading spinner
+            return CircularProgressIndicator();
           },
-          child: Text('Go back!'),
         ),
+
+        //  child: RaisedButton(
+        //       onPressed: () {
+        //        Navigator.pop(context);
+        //     },
+        ///    child: Text('Go back!'),
       ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-    MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -104,7 +166,7 @@ class _MyHomePageState extends State<MyHomePage> {
   String _udid = 'Unknown';
   int _counter = 0;
   String _rank = "";
-  String _friends =  "";
+  String _friends = "";
 
   @override
   void initState() {
@@ -127,42 +189,71 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-
   Future<Response> fetchFriends() {
-    return get('https://jsonplaceholder.typicode.com/posts/1');
+    return get('https://jsonplaceholder.typicode.com/posts/');
   }
-
 
   String getRank(int spentDays) {
+    String rank = "";
 
-  String rank = "";
+    switch (spentDays) {
+      case 1:
+        rank = "Private";
+        break;
+      case 2:
+        rank = "Private";
+        break;
 
-  switch (spentDays) {
-  case 1: rank = "Private"; break;
-  case 2: rank = "Private"; break;
+      case 3:
+        rank = "Fähnrich";
+        break;
+      case 4:
+        rank = "Fähnrich";
+        break;
 
-  case 3: rank = "Fähnrich"; break;
-  case 4: rank = "Fähnrich"; break;
+      case 5:
+        rank = "Leutnant";
+        break;
+      case 6:
+        rank = "Leutnant";
+        break;
+      case 7:
+        rank = "Leutnant";
+        break;
 
-  case 5: rank = "Leutnant"; break;
-  case 6: rank = "Leutnant"; break;
-  case 7: rank = "Leutnant"; break;
+      case 8:
+        rank = "Commander";
+        break;
+      case 9:
+        rank = "Commander";
+        break;
+      case 10:
+        rank = "Commander";
+        break;
 
-  case 8: rank = "Commander"; break;
-  case 9: rank = "Commander"; break;
-  case 10: rank = "Commander"; break;
+      case 11:
+        rank = "Captain";
+        break;
+      case 12:
+        rank = "Captain";
+        break;
+      case 13:
+        rank = "Captain";
+        break;
+      case 14:
+        rank = "Captain";
+        break;
 
-  case 11: rank = "Captain"; break;
-  case 12: rank = "Captain"; break;
-  case 13: rank = "Captain"; break;
-  case 14: rank = "Captain"; break;
+      case 15:
+        rank = "Admiral";
+        break;
+      case 16:
+        rank = "Admiral";
+        break;
+    }
 
-  case 15: rank = "Admiral"; break;
-  case 16: rank = "Admiral"; break;
+    return rank;
   }
-
-  return rank;
-}
 
   void _incrementCounter() {
     setState(() {
@@ -192,7 +283,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: ListView(
         children: [
-
           new Center(
             // Center is a layout widget. It takes a single child and positions it
             // in the middle of the parent.
@@ -220,22 +310,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   'Dein Rang: $_rank',
                   style: Theme.of(context).textTheme.display1,
                 ),
-                FutureBuilder<Post>(
-                  future: fetchPost(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Text(snapshot.data.title);
-                    } else if (snapshot.hasError) {
-                      return Text("${snapshot.error}");
-                    }
-
-                    // By default, show a loading spinner
-                    return CircularProgressIndicator();
-                  },
-                ),
               ],
             ),
-
           ),
           new Card(
             //       child: new AssetImage('res/pics/wiesnplan-2018.jpg'),
@@ -243,11 +319,13 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 const ListTile(
-                  leading: const Icon(Icons.place, color: Colors.green,),
-                //  leading: const Icon(Icons.place, color: Colors.red,),
+                  leading: const Icon(
+                    Icons.place,
+                    color: Colors.green,
+                  ),
+                  //  leading: const Icon(Icons.place, color: Colors.red,),
 
                   title: const Text('Anwesend'),
-
                 ),
                 new Text('$_udid $_friends'),
                 new ButtonTheme.bar(
@@ -261,15 +339,14 @@ class _MyHomePageState extends State<MyHomePage> {
                         },
                       ),
                       new FlatButton(
-                        child: const Text('LeaderBoard'),
+                          child: const Text('LeaderBoard'),
                           onPressed: () {
                             Navigator.push(
                               context,
-                              MaterialPageRoute(builder: (context) => LeaderBoard()),
+                              MaterialPageRoute(
+                                  builder: (context) => LeaderBoard()),
                             );
-                          }
-
-                      ),
+                          }),
                     ],
                   ),
                 ),
